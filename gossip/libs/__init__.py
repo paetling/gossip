72f79038e8af157e9b7005d0782d929e87505de3
paetling <paetling@gmail.com>
2016-01-22 13:21:54 -0500

    small refactor

---
+++ gossip/libs/__init__.py
@@ -0,0 +1,19 @@
+"""
+Based off of this paper: http://docs.hcs.ufl.edu/pubs/GEMS2005.pdf
+Server configs document looks like the following dictionary:
+    {'servers': [{'address': '',
+                  'port': ''},
+                  ...
+                ],
+     'gossip_list': [int, ...],
+     'suspect_list': [int, ...],
+     'suspect_matrix': [[<suspect_list>], ...]}
+'servers': contains how to reach each server
+'gossip_list': contains last time a particular server talked to the other servers
+'suspect_list': contains who a particular server thinks is down
+'suspect_matrix': contains what a particular server thinks all servers think about what servers are down
+                  (it is an aggregation of all servers suspect list)
+NOTE: All lists should be the exact same length
+
+{'current_membership': {'server_configs': server_configs}
+"""
--- gossip/libs/listening_server.py
+++ gossip/libs/listening_server.py
@@ -7,42 +7,36 @@ import yaml
 import random
 
 class ListeningServer(object):
-    def __init__(self, server_index, address, port):
+    def __init__(self, server_index, server_config):
         self.server_index = server_index
-        self.address = address
-        self.port = port
-        self.config_file = get_config_file_name(self.server_index)
+        self.address = server_config['address']
+        self.port = server_config['port']
+        self.config_file_name = get_config_file_name(self.server_index)
 
     def start(self):
         s = socket.socket()
         s.bind((self.address, self.port))
         s.listen()
-        logging.error("Listening on {}".format((self.address, self.port)))
-        if random.randint(0, 15) == 1:
-            logging.error("Killing server {}".format(self.server_index))
-            return
+        logging.int("Listening on {}".format((self.address, self.port)))
         while True:
             from_conn, from_address = s.accept()
-            logging.error("Port {} got a connection from gossiper {}".format(self.port, from_address[1]))
+            logging.info("Port {} got a connection from gossiper {}".format(self.port, from_address[1]))
             while True:
                 data = from_conn.recv(4096).decode('utf-8')
-                logging.error("GOT DATA")
-                logging.error(data)
+                logging.info("GOT DATA")
+                logging.info(data)
                 if MEMBERSHIP_STRING in data:
-                    logging.error("Saving data")
+                    logging.info("Saving data")
                     final_data = yaml.load(data.replace(MEMBERSHIP_STRING, '').replace(STRING_TERMINATOR, ''))
                     self.update_membership(final_data)
                 if STRING_TERMINATOR in data:
-                    logging.error("Ending connection")
+                    logging.info("Ending connection")
                     break
 
     def update_membership(self, new_membership_dict):
-        logging.error("Updating Membership")
-        current_membership_dict = load_membership(self.config_file)
-        logging.error("Current membership is {}".format(current_membership_dict))
+        current_membership_dict = load_membership(self.config_file_name)
         merged_membership_dict = self.merge_membership_dicts(current_membership_dict, new_membership_dict)
-        logging.error("New Membership is {}".format(merged_membership_dict))
-        save_membership(self.config_file, merged_membership_dict)
+        save_membership(self.config_file_name, merged_membership_dict)
 
     def merge_membership_dicts(self, current_membership_dict, new_membership_dict):
         merged_membership = deepcopy(current_membership_dict)
